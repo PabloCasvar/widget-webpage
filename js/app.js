675f26321d47c1185e0f07bb44d6f594e492710c
pabloAnzen <ptovar@anzen.com.mx>
2017-05-09 19:22:32 -0500

    widgets experiencia and formacion added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function(angular){
 
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngSanitize']);
 
 app.component('formacionItem', {
     bindings:{
@@ -9,15 +9,42 @@ app.component('formacionItem', {
         titulo: "@",
         trabajo: "@",
         periodo: "@",
-        imagen: "@"
+        imagen: "@", 
+        pull: "@"
     },
-    controllerAs: 'gridCtrl',
+    controllerAs: 'ctrl',
         controller: function($scope){
+            this.$onInit = function(){
+                this.imagePath = "images/" + this.imagen;
+                 $scope.myHTML =
+     'I am an <code>HTML</code>string with ' +
+     '<a href="#">links!</a> and other <em>stuff</em>';
+            }
             
         },
     templateUrl: 'templates/formacion.tpl.html'
     }
 );
+
+app.component('experienciaItem',{
+    bindings:{
+        empresa: "@",
+        puesto: "@",
+        actividades: "@",
+        link: "@",
+        tipo: "@",
+        periodo: "@",
+        imagen: "@", 
+        pull: "@"
+    },
+    controllerAs: 'ctrl',
+    controller: function($scope){
+        this.$onInit = function(){
+            this.imagePath = "images/" + this.imagen;
+        }
+    }, 
+    templateUrl: "templates/experiencia.tpl.html"
+});
 
 })(window.angular);
 
