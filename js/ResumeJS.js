e617577846ab73b69e287adc9bb7239370977ad6
PabloCasvar <casvar.pablo@gmail.com>
2017-08-03 09:22:56 -0500

    Menu lateral agregado; conflicto carrusel arreglado

--- js/ResumeJS.js
+++ js/ResumeJS.js
@@ -1,42 +1,6 @@
-$( window ).load(function(){
-
-	peticionContenido("/contenido/inicio.html");
-	
-	agregaLinksMenu();
-	ajustaLinksMenu();
-	
-	//agregaScrollMenu();
-	
-	
+$( window ).load(function(){	
+	clickOnLink();
 });
-
-function colocaContenidoPorID(id){
-	fileName = id.toLowerCase().replace("link", "") + ".html";
-	console.log("nombre de archivo: " + fileName);
-	peticionContenido("/contenido/" + fileName)
-	$("#pestana").fadeIn(500);	
-};
-
-function sustituyeLinkActivo(selector){
-	$(".active").removeClass("active");
-        $(selector).addClass("active");
-};
-
-function ajustaLinksMenu(){
-	
-	var acumulador = 0;
-	$(".linkMenu").each(function(index, value){
-		acumulador += $(this).outerWidth();
-	});
-	
-	console.log("ancho acumulado de Links: " + acumulador);
-	var anchoPadre = $(".nav.nav-tabs").width();
-	if(acumulador < anchoPadre){
-	        var numeroLinks = $(".linkMenu").length;
-		var margen = (anchoPadre - acumulador)/(numeroLinks + 1);
-		$(".linkMenu").css({"margin-right": "0", "margin-left": margen.toString() + "px"});
-	}
-};
 
 function agregaScrollMenu(){
         var altura = $("#header").outerHeight(true);
@@ -54,11 +18,14 @@ function agregaScrollMenu(){
 	});
 };
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+};
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+function clickOnLink(){
+	$("#mySidenav a").bind("click", closeNav);
 };
