953595fee7e03272aaf94efdab708552d6163f55
PabloCasvar <casvar.pablo@gmail.com>
2017-08-09 22:56:14 -0500

    Reestructura de archivos

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,25 +6,25 @@ app.config(function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
   $routeProvider
   .when("/", {
-    templateUrl : "templates/inicio.html"
+    templateUrl : "resources/templates/inicio.html"
   })
   .when("/experiencia", {
-    templateUrl : "templates/experiencia.html"
+    templateUrl : "resources/templates/experiencia.html"
   })
   .when("/estudios", {
-    templateUrl : "templates/estudios.html"
+    templateUrl : "resources/templates/estudios.html"
   })
   .when("/cursos", {
-    templateUrl : "templates/cursos.html"
+    templateUrl : "resources/templates/cursos.html"
   })
   .when("/canales", {
-  templateUrl : "templates/canales.html"
+  templateUrl : "resources/templates/canales.html"
   })
   .when("/trabajos", {
-    templateUrl : "templates/trabajos.html"
+    templateUrl : "resources/templates/trabajos.html"
   })
   .when("/curriculum", {
-    templateUrl : "templates/curriculum.html"
+    templateUrl : "resources/templates/curriculum.html"
   });
 });
 
@@ -41,14 +41,14 @@ app.component('formacionItem', {
     controllerAs: 'ctrl',
         controller: function($scope){
             this.$onInit = function(){
-                this.imagePath = "images/" + this.imagen;
+                this.imagePath = "resources/images/" + this.imagen;
                  $scope.myHTML =
      'I am an <code>HTML</code>string with ' +
      '<a href="#">links!</a> and other <em>stuff</em>';
             }
             
         },
-    templateUrl: 'templates/formacion.tpl.html'
+    templateUrl: 'resources/templates/formacion.tpl.html'
     }
 );
 
@@ -66,10 +66,10 @@ app.component('experienciaItem',{
     controllerAs: 'ctrl',
     controller: function($scope){
         this.$onInit = function(){
-            this.imagePath = "images/" + this.imagen;
+            this.imagePath = "resources/images/" + this.imagen;
         }
     }, 
-    templateUrl: "templates/experiencia.tpl.html"
+    templateUrl: "resources/templates/experiencia.tpl.html"
 });
 
 })(window.angular);
